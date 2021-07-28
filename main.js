d28b1725c998d6b57a3d44822b3e9a614c37f63d
jad abdallah <justcallmethis@gmail.com>
2021-07-28 10:03:18 -0700

    finished basic functionality

--- main.js
+++ main.js
@@ -7,20 +7,52 @@ class Block {
         this.previousHash = previousHash;
         this.timeStamp = timeStamp;
         this.hash = this.calculateHash();
+        
     }
 
-    calculateHash(){
+    calculateHash() {
         return SHA256(this.previousHash + this.timeStamp + this.data).toString();
     }
 
+    // what are two reasons you need some kind of algo like pow?
+    // -they could make lots of blocks quickly 
+    // preventing tampering of the past 
 
+    // 51% attack
+    // if someone controls more than half of the network, even with POW, they can rewrite history.
+    // PROOF OF WORK - MINING ALGORITHM
+    mineBlock(difficulty) {
+        // substring and string review
+        // "the cat and the dog".substring(0,2) -> th
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0"))
+            this.nonce++; // 0,1,2,3,4,5,
+            this.hash = this.calculateHash();
+        console.log("BLOCK SUCCESSFULLY MINED: " + this.hash);
+    }
+
+    // minePendingTransactions(miningRewardAddress) {
+    //     // create a new block with pending transactions and mine it...
+    // }
 }
 
-
+// class Transaction {
+//     constructor(fromAddress, toAddress, amount){
+//         this.fromAddress = fromAddress;
+//         this.toAddress = toAddress;
+//         this.amount = amount;
+        
+//     }
+// }
 
 class Blockchain {
-    constructor(){
+    constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
+
+        // // place to store transactions in between creating a block
+        // this.pendingTransactions = [];
+        // // how many coins a miner will receive for their efforts
+        // this.miningReward = 100;
 
     }
 
@@ -32,18 +64,25 @@ class Blockchain {
         // array review
         return this.chain[this.chain.length - 1];
     }
+   
+    // addBlock(newBlock){
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+    // replaces addBlock method 
+    createTransaction(transaction) {
+        // some validation of the transactions here...
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash()
-        this.chain.push(newBlock);
+        // push transaction to pending
+
     }
     // return true or false
     isValid(){
-        for (let i = 1; i < this.chain.length; i++) {
+        for (let i = 1; i < this.chain.length; i++) { 
             const currentBlock = this.chain[i]; // indexing an array (1)
             const previousBlock = this.chain[i-1];
-
+            
             if (currentBlock.hash !== currentBlock.calculateHash())
                 return false;
             if (currentBlock.previousHash !== previousBlock.hash)
@@ -55,8 +94,8 @@ class Blockchain {
 }
 
 let jadCoin = new Blockchain();
-jadCoin.addBlock(new Block("1", "06-21-2021"))
+jadCoin.addBlock(new Block("1", "06-02-2021"))
 
-console.log('is the blockchain valid? ' + jadCoin.isValid());
-jadCoin.chain[1].data = "modified data";
-console.log('is the blockchain still valid? ' + jadCoin.isValid());
+
+// think about how we could validate the Blockchain, implementing basic mining. like if the chain
+// is true return true, false false. Is each block valid and point to the preivous block? 
